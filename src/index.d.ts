8fe335c3c955b8583d38799a67e8132fa8abb253
Mingfun Wong <mingfun.wong.chn@gmail.com>
2023-12-17 12:47:08 +0900

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+import { Api } from 'eosjs/dist/eosjs-api';
+import * as ApiInterfaces from 'eosjs/dist/eosjs-api-interfaces';
+import { JsonRpc } from 'eosjs/dist/eosjs-jsonrpc';
+import * as RpcInterfaces from 'eosjs/dist/eosjs-rpc-interfaces';
+import { RpcError } from 'eosjs/dist/eosjs-rpcerror';
+import * as Serialize from 'eosjs/dist/eosjs-serialize';
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
+
+export { Api, ApiInterfaces, JsonRpc, Numeric, RpcInterfaces, RpcError, Serialize, JsSignatureProvider };
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { Api } from 'eosjs/dist/eosjs-api';
+import * as ApiInterfaces from 'eosjs/dist/eosjs-api-interfaces';
+import { JsonRpc } from 'eosjs/dist/eosjs-jsonrpc';
+import numeric from 'eosjs/dist/eosjs-numeric';
+import * as RpcInterfaces from 'eosjs/dist/eosjs-rpc-interfaces';
+import { RpcError } from 'eosjs/dist/eosjs-rpcerror';
+import * as Serialize from 'eosjs/dist/eosjs-serialize';
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
+
+// https://github.com/FIBOSIO/fibos.js/blob/505c0c35aaf1b62c8dd632cf252e9a1b7d1eba18/lib/patch_numeric.js
+numeric.prefix = "FO";
+
+var stringToPublicKey = numeric.stringToPublicKey;
+numeric.stringToPublicKey = function (s) {
+    if (typeof s !== 'string')
+        throw new Error('expected string containing public key');
+
+    if (s.substr(0, numeric.prefix.length) === numeric.prefix)
+        s = "EOS" + s.substr(numeric.prefix.length);
+
+    return stringToPublicKey(s);
+};
+
+numeric.convertLegacyPublicKey = function (s) {
+    if (s.substr(0, numeric.prefix.length) === numeric.prefix)
+        return numeric.publicKeyToString(numeric.stringToPublicKey(s));
+
+    return s;
+};
+
+var publicKeyToLegacyString = numeric.publicKeyToLegacyString;
+numeric.publicKeyToLegacyString = function (key) {
+    if (key.type === 0 && key.data.length === 33) {
+        var s = publicKeyToLegacyString(key);
+        return numeric.prefix + s.substr(3);
+    }
+    else
+        return publicKeyToLegacyString(key);
+};
+
+const Numeric = numeric;
+
+export { Api, ApiInterfaces, JsonRpc, Numeric, RpcInterfaces, RpcError, Serialize, JsSignatureProvider };
